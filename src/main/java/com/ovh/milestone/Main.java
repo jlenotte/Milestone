0afe244581af09695ce1a27c7febe629e62d5843
jlenotte <jules.lenotte@gmail.com>
2017-08-28 18:42:56 +0200

    Top customers per nic/name functionnal, implemented ParameterTool, still in progress.

--- src/main/java/com/ovh/milestone/Main.java
+++ src/main/java/com/ovh/milestone/Main.java
@@ -1,11 +1,17 @@
 package com.ovh.milestone;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.flink.api.java.operators.GroupReduceOperator;
+import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.operators.ReduceOperator;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.api.java.tuple.Tuple2;
 
 public class Main
@@ -30,40 +36,71 @@ public class Main
 
 
 
-
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
+        // Setup properties
+        String configFile = "milestone.properties";
+        ParameterTool config = ParameterTool.fromPropertiesFile(configFile);
+
 
         // Boilerplate
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        String csvFile = "dataBase.csv";
+
+        //
+        String csvFile = config.get("csvFile", "data.csv");
+        System.out.println(csvFile);
+
+        //
+        String resultFile = config.get("resultFile");
+
+        //
+        String resultCsvFile = config.get("resultCsvFile");
+        System.out.println(resultCsvFile);
+
+        int limit10 = Integer.parseInt(config.get("limit10"));
+        int limit100 = Integer.parseInt(config.get("limit100"));
+        int limit1000 = Integer.parseInt(config.get("limit1000"));
+
 
         // Instances
+
         YearMonthTotal yearMonthTotal = new YearMonthTotal();
         PerNicTotal nicTotal = new PerNicTotal();
+        TopCustomers topCusts = new TopCustomers();
 
         // Read CSV file and convert to POJO
         DataSet<Invoice> data = env.readCsvFile(csvFile)
                                    .pojoType(Invoice.class, "nichandle", "name", "firstName", "transaction", "date");
 
+
         // Try to run the code
         try
         {
+
             // Get the total of transactions per nichandle
-            // MapOperator<Invoice, Tuple2<String, Double>> result = nicTotal.getNichandleSumFlink(data);
+            MapOperator<Invoice, Tuple2<String, Double>> result = nicTotal
+                .getNichandleSumFlink(data);
 
             // Get the sum of all transactions per year/MM
-            ReduceOperator<Tuple2<String, Double>> result = yearMonthTotal.getTotalPerYearMonth(data);
+            // ReduceOperator<Tuple2<String, Double>> result = yearMonthTotal.getTotalPerYearMonth(data);
+
+            // Get top customers
+            // GroupReduceOperator result = topCusts.getTopCustomersByNic(data, limit100);
+
+            // Get top months
+            // GroupReduceOperator result = yearMonthTotal.getBestMonths(data, 100);
+
 
             // Get the result in a DataSink
-            result.writeAsText("/home/jlenotte/WORKSPACE/Milestone/toto/", FileSystem.WriteMode.OVERWRITE);
+            result.writeAsText(resultCsvFile, FileSystem.WriteMode.OVERWRITE);
 
-            // Execute
-            env.execute();
         }
         catch (Exception e)
         {
             LOGGER.error(e.getMessage());
         }
+
+        // Execute
+        env.execute();
     }
 }
--- src/main/java/com/ovh/milestone/TopCustomers.java
+++ src/main/java/com/ovh/milestone/TopCustomers.java
@@ -1,18 +1,15 @@
 package com.ovh.milestone;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.operators.GroupReduceOperator;
-import org.apache.flink.api.java.operators.ReduceOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class YearMonthTotal
+public class TopCustomers
 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(YearMonthTotal.class);
@@ -20,74 +17,33 @@ public class YearMonthTotal
 
 
     /**
-     * Get total profit of each month per year
+     * Get the 100 best customers (name + transaction)
+     *
+     * @param data Takes a DataSet in
+     * @param limit limits the operation to the 100 first elements
+     * @return Returns a GroupReduceOperator
      */
-    public ReduceOperator<Tuple2<String, Double>> getTotalPerYearMonth(DataSet<Invoice> data)
+    public GroupReduceOperator getTopCustomers(DataSet<Invoice> data, int limit)
     {
-        ReduceOperator<Tuple2<String, Double>> result = null;
+        GroupReduceOperator result = null;
 
         try
         {
             result = data
-                // Map from Invoice -> Date & transaction
+                // Map from Invoice to Name + transaction
                 .map(new MapFunction<Invoice, Tuple2<String, Double>>()
                 {
                     @Override
                     public Tuple2<String, Double> map(Invoice value) throws Exception
                     {
-                        ZonedDateTime zDate = value.getZonedDate();
-                        String s = zDate.format(DateTimeFormatter.ofPattern("yyyy/MM"));
-                        Double amount = value.getTransaction();
-                        return new Tuple2<>(s, amount);
+                        String custName = value.getName() + " " + value.getFirstName();
+                        Double transaction = value.getTransaction();
+                        return new Tuple2<>(custName, transaction);
                     }
                 })
-                // Group dates by year/month (here we group by the Tuple2's index 0
+                // GroupBy customer's name (Tuple's field 0)
                 .groupBy(0)
-                // Reduce to one value (a month of the year)
-                .reduce(new ReduceFunction<Tuple2<String, Double>>()
-                {
-                    @Override
-                    public Tuple2<String, Double> reduce(Tuple2<String, Double> value1, Tuple2<String, Double> value2) throws Exception
-                    {
-                        Double sum = value1.f1 + value2.f1;
-                        return new Tuple2<>(value1.f0, sum);
-                    }
-                });
-        }
-        catch (Exception e)
-        {
-            LOGGER.error(e.getMessage());
-        }
-        return result;
-    }
-
-
-
-    /**
-     * Get the best months
-     */
-    public GroupReduceOperator<Tuple2<String, Double>, Tuple2<String, Double>> getBestMonths(DataSet<Invoice> data, int limit)
-    {
-        GroupReduceOperator<Tuple2<String, Double>, Tuple2<String, Double>> result = null;
-
-        try
-        {
-            result = data
-                // Map from Invoice -> Date & transaction
-                .map(new MapFunction<Invoice, Tuple2<String, Double>>()
-                {
-                    @Override
-                    public Tuple2<String, Double> map(Invoice value) throws Exception
-                    {
-                        ZonedDateTime zDate = value.getZonedDate();
-                        String s = zDate.format(DateTimeFormatter.ofPattern("yyyy/MM"));
-                        Double amount = value.getTransaction();
-                        return new Tuple2<>(s, amount);
-                    }
-                })
-                // Group dates by year/month (here we group by the Tuple2's index 0
-                .groupBy(0)
-                // Reduce to one value (a month of the year)
+                // Reduce
                 .reduce(new ReduceFunction<Tuple2<String, Double>>()
                 {
                     @Override
@@ -98,12 +54,68 @@ public class YearMonthTotal
                     }
                 })
                 .sortPartition(1, Order.DESCENDING)
-                .first(10);
+                .first(limit);
         }
         catch (Exception e)
         {
             LOGGER.error(e.getMessage());
         }
+
+        return result;
+    }
+
+
+
+    /**
+     * Get the 100 best customers (nic + transaction)
+     *
+     * @param data Takes a DataSet in
+     * @param limit limits the operation to the 100 first elements
+     * @return Returns a GroupReduceOperator
+     */
+    public GroupReduceOperator<Tuple2<String, Double>, Tuple2<String, Double>> getTopCustomersByNic(DataSet<Invoice> data, int limit)
+    {
+        GroupReduceOperator<Tuple2<String, Double>, Tuple2<String, Double>> result = null;
+
+        try
+        {
+            result = data
+                // Map from Invoice to Name + transaction
+                .map(new MapFunction<Invoice, Tuple2<String, Double>>()
+                {
+                    @Override
+                    public Tuple2<String, Double> map(Invoice value) throws Exception
+                    {
+                        String nic = value.getNichandle()
+                            + "("
+                            + value.getName()
+                            + " "
+                            + value.getFirstName()
+                            + ")";
+                        Double transaction = value.getTransaction();
+                        return new Tuple2<>(nic, transaction);
+                    }
+                })
+                // GroupBy customer's name (Tuple's field 0)
+                .groupBy(0)
+                // Reduce
+                .reduce(new ReduceFunction<Tuple2<String, Double>>()
+                {
+                    @Override
+                    public Tuple2<String, Double> reduce(Tuple2<String, Double> value1, Tuple2<String, Double> value2) throws Exception
+                    {
+                        Double sum = value1.f1 + value2.f1;
+                        return new Tuple2<>(value1.f0, sum);
+                    }
+                })
+                .sortPartition(1, Order.DESCENDING)
+                .first(limit);
+        }
+        catch (Exception e)
+        {
+            LOGGER.error(e.getMessage());
+        }
+
         return result;
     }
 }
